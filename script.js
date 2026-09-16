a5ccb414ca23abe8dad98158f43492b306ff441b
Jéssica Rodrigues de Oliveira <jessrodrigues0201@gmail.com>
2026-09-16 12:54:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Menu hamburguer //
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.getElementById('menuToggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  if (menuToggle && navLinks) {
+    menuToggle.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+    });
+  }
+});
